6da0c2dc1747e6ee12f299a22e9aaf0b9d8e5162
nanajulieaugustine <naje0004@stud.kea.dk>
2025-05-12 20:24:31 +0200

    nana: "vis flere" knap virker, så der kun vises 30 værker ad gangen

--- src/librery/artworkUtils.js
+++ src/librery/artworkUtils.js
@@ -1,3 +1,7 @@
-export const filterHasImage = (artworks) => artworks.filter(item => item.has_image);
-export const limitTo30 = (artworks) => artworks.slice(0, 30);
-export const visFlere = (artworks, offset) => artworks.slice(offset, offset + 30);
+export const filterHasImage = (artworks) => artworks.filter(item => item.has_image); //vis kun værker med img 
+export const limitTo30 = (artworks) => artworks.slice(0, 30); //vis kun 30 værker ad gangen
+
+
+// &&
+//     item.object_names?.some(obj => obj.name === "Skulptur") // display evt. efter type kunst???
+//måske enrichment_url, beskrivelse? et eller andet, der giver flere informationer? 
