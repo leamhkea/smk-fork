0ac6a1ca9a95a9063cb91160159a415b62a7a76c
leamhkea <lehe0001@stud.kea.dk>
2025-05-20 11:04:30 +0200

    Lea: hurtige noter til listserver

--- src/components/offentlig/listview/ListServer.jsx
+++ src/components/offentlig/listview/ListServer.jsx
@@ -1,7 +1,12 @@
 import ListClient from "./ListClient";
 
+// ListServer er en asynkron komponent. async betyder, at man arbejder med asynkrone operationer (f.eks. at hente data fra en API).
 const ListServer = async () => {
+  // await: Da fetch er en asynkron funktion, bruges await til at vente på, at anmodningen bliver færdig, før man går videre.
+  // fetch: Bruges til at sende en HTTP-anmodning til en API. Her sendes en GET-anmodning til Petfinder API
   const event = await fetch("http://localhost:8080/events");
+
+  // Når fetch-anmodningen er færdig, fås et Response-objekt (data), som konverteres til et JSON-format.
   const events = await event.json();
 
   // Herunder laves const til alle lokationer, som bliver filtreret
