4d6f72187d2b533f76b0f73786b12eff811c425a
nanajulieaugustine <naje0004@stud.kea.dk>
2025-05-26 13:21:59 +0200

    Nana: viser kun datoer, der er gældende fra api'et og disabler de datoer, der allerede er valgt på samme lokation - samt disable kunstværker hvis den er valgt på en lokation (skal opdateres til at tale sammen med datoer)

--- src/components/global/buttons/TertrieryButton.jsx
+++ src/components/global/buttons/TertrieryButton.jsx
@@ -1,6 +1,6 @@
 const TertrieryButton = ({ children, onClick }) => {
   return (
-    <button onClick={onClick} className="z-10 py-3 px-4 flex gap-2 text-center border-1 text-(--blue) border-(--blue) items-center w-full justify-center mt-5 hover:underline  hover:scale-103 transition-all duration-300">
+    <button onClick={onClick} className="py-3 px-4 flex gap-2 text-center border-1 text-(--blue) border-(--blue) items-center w-full justify-center mt-5 hover:underline  hover:scale-103 transition-all duration-300">
       {children}
     </button>
   );
