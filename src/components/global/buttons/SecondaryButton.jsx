710e2cc4ea7ad58bf3628f62c64aa4a60b2e7be7
nanajulieaugustine <naje0004@stud.kea.dk>
2025-06-01 16:41:45 +0200

    Nana: oprydning

--- src/components/global/buttons/SecondaryButton.jsx
+++ src/components/global/buttons/SecondaryButton.jsx
@@ -1,6 +1,7 @@
-const SecondaryButton = ({ children, onClick }) => {
+const SecondaryButton = ({ children, onClick, disabled }) => {
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
       className="py-3 px-4 flex gap-2 text-center border-1 text-(--blue) border-(--blue) items-center w-full justify-center mt-5 hover:underline  hover:scale-103 transition-all duration-300"
     >
