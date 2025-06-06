173d837ae5746304dab2ff44f27b97c332f649c5
leamhkea <lehe0001@stud.kea.dk>
2025-06-06 12:18:13 +0200

    Lea: ved ikke hvor mange forsøg jeg er oppe på nu

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>SMK - Statens Museum for Kunst</title>
+      <meta name="description" content="SMK arrangement side" />
+    </>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import "./reset.css";
 import Header from "@/components/global/Header";
 import Footer from "@/components/global/Footer";
-import Head from "next/head";
 
 //imports udefra
 import { ClerkProvider } from "@clerk/nextjs";
@@ -20,9 +19,6 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="da">
-        <Head>
-          <meta charSet="UTF-8" />
-        </Head>
         <body>
           <header>
             <Header />
