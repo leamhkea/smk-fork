8644e1a826bae1620dfc5d2824aa84afa1237538
leamhkea <lehe0001@stud.kea.dk>
2025-06-09 12:02:26 +0200

    Lea: ordnet server ssr:false

--- src/app/arrangementer/[id]/page.jsx
+++ src/app/arrangementer/[id]/page.jsx
@@ -3,9 +3,7 @@ import dynamic from "next/dynamic";
 const SingleCard = dynamic(
   () => import("@/components/offentlig/singleview/SingleCard"),
   {
-    ssr: false, // Hvis komponenten kun skal køre i browseren
     loading: () => <p>Loading...</p>,
-    ssr: false,
   }
 );
 
